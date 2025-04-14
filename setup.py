b9338b0b7203cb0c69f034be124b21ad5c378e41
Sakshi chaudhary <chaudhary90sakshi@gmail.com>
2025-04-14 20:11:15 +0530

    folder structure added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages,setup
+
+setup(
+  name = 'Agriculture chatbot project',
+  version='0.0.0',
+  author='Sakshi',
+  author_email= 'chaudharyasakshi@gmail.com',
+  packages= find_packages(),
+  install_requires = []
+)
